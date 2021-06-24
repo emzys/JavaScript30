9df89c6c11bee1eb276dc9832c022441ae31eba7
Emilia Zysk <emzysk.dev@gmail.com>
2021-06-24 19:37:12 +0900

    add my solution

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,20 @@
+const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`div[data-key="${e.key}"]`);
+  if (!audio) return
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+};
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
